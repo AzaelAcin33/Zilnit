7d9344f1c8d7f5c2831b4ad40b8f5bd505d8d444
AzaelAcin33 <pitilin2424@gmail.com>
2026-06-18 18:28:55 +0200

    FEAT: Rutas protegidas

--- zelnit-api/src/auth/auth.module.ts
+++ zelnit-api/src/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     PrismaModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'zelnit-super-secret',
       signOptions: {
@@ -14,7 +17,7 @@ import { AuthService } from './auth.service';
       },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService]
 })
--- zelnit-api/src/users/users.controller.ts
+++ zelnit-api/src/users/users.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/jwt.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(
         private readonly usersService: UsersService,
-    ) {}
+    ) { }
 
     @Get()
     findAll() {
         return this.usersService.findAll();
+    }
+    @UseGuards(JwtGuard)
+    @Get('me')
+    me(@Req() req: any) {
+        return req.user;
     }
 }
