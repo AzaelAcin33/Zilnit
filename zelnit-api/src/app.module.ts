0d39a1706d8e0bd9fc29aacdd2a769b99f389a79
AzaelAcin33 <pitilin2424@gmail.com>
2026-06-19 14:04:17 +0200

    FEAT: Sistema para conseguir categorias hecho

--- zelnit-api/src/app.module.ts
+++ zelnit-api/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, CategoriesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
